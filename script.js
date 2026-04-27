ef0315137ca67e95b147769eea3897f1aefe93f5
студент <studentnezalko0@gmail.com>
2026-04-27 12:32:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let requestCount = 0;
-let GROQ_API_KEY = "    ";
+let GROQ_API_KEY = "  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  ";
 
 function setupAPIKey() {
     const savedKey = localStorage.getItem('groq_api_key');
